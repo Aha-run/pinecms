eb2957c5ba62f6918893e16405feb161b0b3eb09
xiusin <826466266@qq.com>
2024-05-20 22:13:57 +0800

    优化

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/xiusin/pinecms
 
-go 1.19
+go 1.21
+
+toolchain go1.22.2
 
 require (
 	github.com/CloudyKit/jet v2.1.3-0.20180809161101-62edd43e4f88+incompatible
--- src/common/search/bleve.go
+++ src/common/search/bleve.go
@@ -9,9 +9,12 @@ import (
 
 const ServiceSearchName = "search"
 
+// https://vimsky.com/examples/detail/golang-ex-github.com.blevesearch.bleve-Index-Search-method.html
+// https://studygolang.com/articles/36305
+
 func NewBleve() {
 	mapping := bleve.NewIndexMapping()
-	mapping.DefaultAnalyzer = "en"
+	mapping.DefaultAnalyzer = "cn"
 
 	index, err := bleve.New("example.bleve", mapping)
 	helper.PanicErr(err)
