1deb7e66eade76f01204f32cc4f40b2b60b42513
xiusin <826466266@qq.com>
2024-05-29 07:15:44 +0800

    搜索引擎实现

--- src/common/search/blevesearch.go
+++ src/common/search/blevesearch.go
@@ -1,1 +1,20 @@
 package search
+
+type BleveSearch struct {}
+
+func (b *BleveSearch) Search(index string, query any) (any, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (b *BleveSearch) Index(index string, doc map[string]any) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (b *BleveSearch) Update(index string, id string, doc map[string]any) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (b *BleveSearch) Delete(index string, id string) error {
+	panic("not implemented") // TODO: Implement
+}
+
